98131ab82d6f26268ea74d6945750f3589d305c4
Anton Fisher <a.fschr@gmail.com>
2016-03-29 23:30:07 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 import 'es6-shim';
 import 'es6-promise';
+import 'babel-polyfill';
 import 'angular2/bundles/angular2-polyfills'
-import {provide} from 'angular2/core';
-import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {provide} from 'angular2/core'
+import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router'
 import {bootstrap} from 'angular2/platform/browser'
 
 import {AppComponent} from './app/app.component'
-import {LocalStorageService} from './app/services/localStorage.service'
 
 bootstrap(
   AppComponent,
   [
-    LocalStorageService,
     ROUTER_PROVIDERS,
     provide(APP_BASE_HREF, {useValue: '/'}),
     provide(LocationStrategy, {useClass: HashLocationStrategy})
   ]
-);
+)
